0c8b6826042965e318d95da28b53811a9de6c202
Enoch <enochbabatunde17@gmail.com>
2025-04-03 16:09:33 +0100

    updated shits

--- scripts.js
+++ scripts.js
@@ -1,1 +1,37 @@
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+       el: '.swiper-pagination',
+       clickable: true,
+       dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerview: 1
+
+      },
+      
+    //   620: {
+    //     slidesPerView: 2
+
+    //   },
+    //   1024: {
+    //     slidesPerView: 3
+
+    //   },
+
+    }
+  });
+
 alert('Welcome')
